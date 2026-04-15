019cfafa4b3fb02e22b87a14357da4f47f57f5b3
Oded Hogeg <oded@Odeds-Mac-mini.local>
2026-04-15 08:04:38 -0700

    Add custom domain chipipay.co

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/chipi-payment-solution",
-  assetPrefix: "/chipi-payment-solution",
   images: {
     unoptimized: true,
   },
